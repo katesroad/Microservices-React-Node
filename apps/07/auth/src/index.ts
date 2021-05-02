8c9dde7041432d215e759116f8186694eccef4d1
yup <yup@gmail.com>
2021-05-01 23:16:23 -0400

    section7

---
+++ apps/07/auth/src/index.ts
@@ -0,0 +1,27 @@
+import { json } from 'body-parser';
+import express from 'express';
+import 'express-async-errors';
+import { NotFoundError } from './errors/not-found-error';
+import { errorHandler } from './middlewares/error-handler';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
+const app = express();
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!!!!!!!!');
+});
