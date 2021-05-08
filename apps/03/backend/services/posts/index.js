7498cc6fc4fb8ed52523ad5ba57a38f9152a0908
Kate <test@gmail.com>
2021-05-08 15:55:30 -0400

    update localhost as service url

--- apps/03/backend/services/posts/index.js
+++ apps/03/backend/services/posts/index.js
@@ -20,7 +20,7 @@ app.post("/posts", async (req, res) => {
 
   posts[id] = { id, title };
 
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv", {
     type: "PostCreated",
     data: { id, title },
   });
