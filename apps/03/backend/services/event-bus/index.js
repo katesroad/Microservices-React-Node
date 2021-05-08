3cf0ed8fb5fbfa96622194f6d936b153afc32a69
Kate <test@gmail.com>
2021-05-08 16:03:06 -0400

    update localhost as service url to posts service

--- apps/03/backend/services/event-bus/index.js
+++ apps/03/backend/services/event-bus/index.js
@@ -12,7 +12,7 @@ app.post('/events', (req, res) => {
 
   events.push(event);
 
-  axios.post('http://localhost:4000/events', event);
+  axios.post('http://posts-clusterip-srv:4000/events', event);
   axios.post('http://localhost:4001/events', event);
   axios.post('http://localhost:4002/events', event);
   axios.post('http://localhost:4003/events', event);
