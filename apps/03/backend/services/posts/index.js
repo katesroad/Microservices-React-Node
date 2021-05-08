93413b676192707e03c33147099c704feb0a546e
Kate <test@gmail.com>
2021-05-08 16:13:26 -0400

    should add port for accessing event-bus container

--- apps/03/backend/services/posts/index.js
+++ apps/03/backend/services/posts/index.js
@@ -20,7 +20,7 @@ app.post("/posts", async (req, res) => {
 
   posts[id] = { id, title };
 
-  await axios.post("http://event-bus-srv", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: { id, title },
   });
